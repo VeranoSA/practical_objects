e303439009ea266cb7e01999091adaaa00420287
Tebogo Seanego <50863472+VeranoSA@users.noreply.github.com>
2020-11-18 22:05:22 +0200

    Create findItemsOver.js

---
+++ findItemsOver.js
@@ -0,0 +1,17 @@
+function findItemsOver(fruitList, thresh){
+var overThresh = [];
+  
+  for(i in fruitList){
+  if(fruitList[i].qty > thresh){
+  		overThresh.push(fruitList[i]);
+  }
+  }
+  return overThresh;
+}
+var itemList = [
+    {name : 'apples', qty : 10},
+    {name : 'pears', qty : 37},
+    {name : 'bananas', qty : 27},
+    {name : 'apples', qty : 3},
+];
+console.log(findItemsOver(itemList, 4));
